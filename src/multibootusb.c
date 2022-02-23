5dd9053f14bcb410981c7f58ff088a3c9162fcb7
Dimitris Tzemos <djemos@users.noreply.github.com>
2022-02-23 21:25:54 +0200

    Update multibootusb.c

--- src/multibootusb.c
+++ src/multibootusb.c
@@ -2,9 +2,11 @@
 multibootusb.c from Slackel <http://www.slackel.gr>
 Copyright (C) 2019 Dimitris Tzemos <dijemos@gmail.com>
 
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License at <http://www.gnu.org/licenses/> for more details.
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation,
+either version 2 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License at <http://www.gnu.org/licenses/> for more details.
 */
 
 #include <gtk/gtk.h>
